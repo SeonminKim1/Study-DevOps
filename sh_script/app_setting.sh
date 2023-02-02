9bdfc9cdbef2ca16f7635589b90ddf4f9fb7431c
SeonminKim1 <yubi5050@naver.com>
2023-02-02 16:20:34 +0900

    Add sh scripts

---
+++ sh_script/app_setting.sh
@@ -0,0 +1,8 @@
+# project settings
+sudo apt-get -y update 
+sudo apt-get -y upgrade
+mkdir moagudok && cd moagudok
+git init
+git remote add origin https://github.com/nabacamp-side/side1-be.git
+git pull
+git checkout production
